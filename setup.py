4bb5b2ae849831d95ff974f33ec0e0dcbc1ab04f
Simon Weber <simon@simonmweber.com>
2016-01-10 12:30:55 -0500

    add 3.4/3.5 compatibility explicitly
    see https://github.com/simon-weber/gpsoauth/pull/8#issuecomment-170372589

--- setup.py
+++ setup.py
@@ -49,5 +49,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ),
 )
