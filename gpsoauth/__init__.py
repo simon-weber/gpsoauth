b22c57a236eb0bd63517cb4c1bf35d3fca35e2c7
Simon Weber <simon@simonmweber.com>
2023-06-01 21:18:38 -0400

    appease linters

--- gpsoauth/__init__.py
+++ gpsoauth/__init__.py
@@ -11,12 +11,14 @@ import requests
 # Type annotations for urllib3 will be released with v2.
 from urllib3.poolmanager import PoolManager  # type: ignore[import]
 
+from . import google
+
 SSL_DEFAULT_CIPHERS = None
 if version("urllib3") < "2.0.0a1":
     from urllib3.util.ssl_ import DEFAULT_CIPHERS  # type: ignore[import]
+
     SSL_DEFAULT_CIPHERS = DEFAULT_CIPHERS
 
-from . import google
 
 __version__ = version(__package__)
 
